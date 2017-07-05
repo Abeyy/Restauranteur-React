4e1a980782f19516ee7c8da3b3ca97434cce799c
Abeyy <abey.joy@nyu.edu>
2017-07-04 22:18:15 -0400

    init commit, added boostrap via CDN

---
+++ src/components/Single.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Single extends Component {
+    render () {
+        return (
+        <div className="panel">
+            <h1> Hello Bitches </h1>
+        </div>
+
+        );
+    }
+}
+
+export default Single;
--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,6 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Single from './Components/Single.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(<Single />, document.getElementById('react-root'));
